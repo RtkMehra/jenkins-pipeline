fc23410ed2d77e38983b67e7338384495c16f878
Ritik <mehra.ritik18@gmail.com>
2024-06-15 02:58:45 +0530

    port fix

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const logger = new Logger('Bootstrap');
   
   // Log a starting server message
-  const port = 3000;
+  const port = process.env.PORT || 3000;
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
 }
